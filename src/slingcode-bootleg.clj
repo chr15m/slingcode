90019bb99044a0d6bf2b85efd1568040ce7b30ec
Chris McCormick <chris@mccormick.cx>
2020-04-26 20:31:40 +0800

    More tightly target script entrypoint.

--- src/slingcode-bootleg.clj
+++ src/slingcode-bootleg.clj
@@ -6,4 +6,4 @@
       (enlive/at [:link#style] (enlive/substitute (convert-to [:style css] :hickory)))
       (enlive/at [:link.css] (enlive/substitute nil))
       (enlive/at [:link#favicon] (enlive/substitute (convert-to [:link {:href logo}] :hickory)))
-      (enlive/at [:script] (enlive/substitute (convert-to [:script js] :hickory)))))
+      (enlive/at [:script#entrypoint] (enlive/substitute (convert-to [:script js] :hickory)))))
